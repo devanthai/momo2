61c3a5f2b3b216c3bc8e23032d1ec270c8a22aef
devanthai <thaile2462@gmail.com>
2022-10-19 13:59:07 +0700

    dfg

--- controllers/autoMomo.js
+++ controllers/autoMomo.js
@@ -805,6 +805,7 @@ async function CheckGd(phone, dateString, setting, limit = 20) {
 
     try {
         let hiss = await MomoService.getTranshis(phone.phone, dateString, dateString, limit)
+        console.log(hiss)
         console.log("checkgd2 "+phone.phone +"  "+hiss.length)
 
         let zz = hiss
